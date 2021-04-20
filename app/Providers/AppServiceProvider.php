515f2d1f2b694874de2f832692646b2b44265b58
Drago Stevanovic <drago.stevanovic@coreware.us>
2021-04-21 01:22:13 +0200

    show latest movies in side bar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Movie;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $latest_movies = Movie::orderBy('created_at', 'desc')->take(5)->get();
+        View::share('latest_movies', $latest_movies);
     }
 }
